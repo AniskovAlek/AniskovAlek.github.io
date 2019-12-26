be5070f86230933751ad63361c271d0fe7c98b32
AniskovAlex <54923203+AniskovAlex@users.noreply.github.com>
2019-12-26 11:46:17 +0700

    яприапара
    вапавп

--- Woozer/script.js
+++ Woozer/script.js
@@ -4,3 +4,33 @@ function menu(){
   else
   d1.setAttribute('show','false');
 }
+
+function load() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    myFunction(this);
+    }
+  };
+  xhttp.open("GET", "file.xml", true);
+  xhttp.send();
+}
+
+function blogs(xml){
+  var i;
+  xmlDoc= xml.responseXML;
+  var str;
+  x=xmlDoc.getElementByTagName("hgroup");
+  for(i=0;i<x.length;i++){
+    str+="<hgroup class=&quot;blog&quot;> <span>"+
+    x[i].getElementsByTagName("span")[0].childNodes[0].nodeValue +
+    "</span><header class=&quot;title&quot;>" +
+    x[i].getElementsByTagName("header")[0].childNodes[0].nodeValue +
+    "</header><article class=&quot;blog_post&quot;>"+
+    x[i].getElementsByTagName("article")[0].childNodes[0].nodeValue+
+    "</article><a href= "+
+    x[i].getElementsByTagName("a")[0].childNodes[0].nodeValue+
+    ">Подробнее</a></hgroup>";
+  }
+  document.getElementById("con").innerHTML = str;
+}
