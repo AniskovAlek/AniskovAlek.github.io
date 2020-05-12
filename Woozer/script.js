142c4f9c1243cd29a7e94f378117308fd2bf4bf7
AniskovAlex <54923203+AniskovAlex@users.noreply.github.com>
2020-05-12 20:16:50 +0700

    Версия 0.8
    очень много интересного, но не сейчас...

--- Woozer/script.js
+++ Woozer/script.js
@@ -1,57 +1,82 @@
-function menu(){
-  if(d1.getAttribute('show')=='false')
-  d1.setAttribute('show','true');
-  else
-  d1.setAttribute('show','false');
+var app = angular.module('myApp', []);
+app.controller('customersCtrl', function($scope, $http) {
+$http.get("http://26.17.129.233:1337/?query=list")//26.17.129.233
+.then(function (response) {
+	$scope.bl = response.data;
+});
+
+$scope.id_arr=[];
+
+$scope.test=function(event){
+	$scope.id_arr.push(event.target.value);
+	alert($scope.id_arr);
 }
 
-function load() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-    var buf = JSON.parse(this.responseText);
-    var myObj= JSON.parse(buf);
-    alert(myObj);
-    blogs_in(myObj);
-    }
-  };
-  xhttp.open("GET", "http://localhost:1337/", true);
-  xhttp.send();
+$scope.x_row=50;
+
+$scope.show_content=true;
+$scope.show_creat_blog=false;
+$scope.show_users_fun=false;
+
+$scope.littlefun=function(){
+	alert();
+	$scope.show_users_fun=!$scope.show_users_fun;
 }
 
-function con_to_json(xml){
-  xmlDoc= xml.response;
-  document.getElementById("con").innerHTML = xml.responseText;
-  alert(xmlDoc);
-  x=xmlDoc.getElementsByTagName("hgroup");
-  var blogs=[];
-  var i;
-  for(i=0;i<x.length;i++){
-    var blog={
-      span: x[i].getElementsByTagName("span")[0].childNodes[0].nodeValue,
-      time: x[i].getElementsByTagName("time")[0].childNodes[0].nodeValue,
-      header: x[i].getElementsByTagName("header")[0].childNodes[0].nodeValue,
-      article: x[i].getElementsByTagName("article")[0].childNodes[0].nodeValue,
-      a: x[i].getElementsByTagName("a")[0].childNodes[0].nodeValue
-    }
-    blogs.push(blog);
-  }
-  blogs_in(blogs);
+$scope.add_blog=function(id,header,text){
+	var str="http://localhost:1337/?query=add_blog&id="+id+"&header="+header+"&text="+text;
+	$http.get(str)
+	.then(function (response) {
+	});
+};
+
+$scope.to_creat_blog=function(){
+$scope.show_content=false;
+$scope.show_creat_blog=true;
 }
 
-function blogs_in(json){
-  var i;
-  var blogs=[];
-  blogs = json.blogs;
-  var str="";
-  for(x in blogs){
-    str+="<div class=blog>"+
-    "<span>"+blogs[x].span+"</span>"+
-    "<time>"+blogs[x].time+"</time>"+
-    "<header>"+blogs[x].header+"</header>"+
-    "<article>"+blogs[x].article+"</article>"+
-    "<a href ="+blogs[x].a+">Подробнее</a>"+
-    "</div>";
-  }
-  document.getElementById("con").innerHTML = str;
+$scope.show_input_login=false;
+$scope.myfunction=function(){
+$scope.show_input_register=false;
+$scope.show_input_login=!$scope.show_input_login;
 }
+
+$scope.show_input_register=false;
+$scope.register=function(){
+$scope.show_input_login=false;
+$scope.show_input_register=!$scope.show_input_register;
+}
+
+$scope.logining=function(login,password){
+	var str="http://localhost:1337/?query=login&login="+login+"&password="+password;
+	$http.get(str)
+	.then(function (response) {
+		alert();
+		$scope.pers=response.data;
+		$scope.auth=$scope.pers.id;
+		$scope.show_input_login=false;
+	});
+};
+
+$scope.registering=function(login,password){
+	var str="http://localhost:1337/?query=register&login="+login+"&password="+password;
+	$http.get(str)
+	.then(function (response) {
+		$scope.pers=response.data;
+		$scope.auth=$scope.pers.id;
+		$scope.show_input_register=false;
+	});
+};
+
+$scope.ttt = function(keyEvent) {
+  if (keyEvent.which === 13)
+	$http.get("http://localhost:1337/?query=search&str="+keyEvent.target.value)
+	.then(function (response) {
+		$scope.bl = response.data;
+	});
+}
+
+$scope.test=function(){ alert();};
+
+
+});
